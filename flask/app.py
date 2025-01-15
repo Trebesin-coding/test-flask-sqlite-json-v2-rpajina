f0f2029bda26504e4686b3ae7f6e832b33968d70
rpajina <rosikova121@trebesin.cz>
2025-01-15 09:11:31 +0100

    test

--- flask/app.py
+++ flask/app.py
@@ -1,8 +1,36 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("vitej.html")
+
+@app.route("/form")
+def form():
+    recenze = request.args.get("recenze")
+    print(recenze)
+    message = None
+
+    if recenze == "nic":
+        message = "uzivatel byl moc liny na napsani recenze"
+        print(message)
+        recenze = message
+
+    elif len(recenze) <= 3:
+        message = "uzivatel byl moc liny na napsani recenze"
+        print(message)
+        recenze = message
+        
+
+    return render_template("form.html", recenze=recenze, message = message)
+
+
+
 # Z následujících si vyber kód a sestav funkční flask aplikaci (není třeba použít vše, vyber si pouze ty části kódu, které potřebuješ)
 # Kód je funkční, pouze místo dotazníků je potřeba doplnit podle potřeby
 
 
-# app = Flask(__name__)
 # from flask import Flask, render_template, request, redirect, url_for
 # @app.route("???")
 # request.args.get("???")
@@ -12,5 +40,5 @@
 # if request.method == "POST"
 
 
-if __name__ == "__main__":
+if __name__ == "__main__": #aplikace se spusti pouze, pokud je spoustena jako hlavni, nespusti se kdyz je importovany modul
     app.run(debug=True)
--- sqlite/test_sqlite.py
+++ sqlite/test_sqlite.py
@@ -1,20 +1,19 @@
 import sqlite3
 
 # Proč je connection podtržené? Oprav chybu
-cursor = connection.cursor()
 
 # Co tu chybí?
-connection = sqlite3.connect("???")
+connection = sqlite3.connect("filmy.db")
+
+cursor = connection.cursor()
+
+int_film = input("Jméno filmu: ")
+int_hodnoceni = int(input("Hodnocení filmu ?/10: "))
 
 
 # Oprav vytváření tabulky - hodnocení je číselné
 cursor.execute(
-    """CREATE TABLE IF NOT EXISTS hodnoceni (
-        id PRIMARY KEY,
-        nazev_filmu,
-        hodnoceni
-    )
-    """
+    'CREATE TABLE IF NOT EXISTS hodnoceni (id INTEGER PRIMARY KEY,nazev_filmu,hodnoceni INTEGER)'
 )
 
 # Zapsání do databáze
@@ -22,15 +21,19 @@ cursor.execute(
 # tady by měl vepsat hodnocení do databáze - není potřeba využívat input, stačí zapsat statický údaj
 # cursor.???
 
+cursor.execute('INSERT INTO hodnoceni (nazev_filmu, hodnoceni) VALUES (?,?)', (int_film, int_hodnoceni))
+
 connection.commit()
 
 
 # Vypisování hodnocení
 
-cursor.execute("???")
+cursor.execute("SELECT * FROM hodnoceni")
 
 rows = cursor.fetchall()
 
 # tady printne hodnocení filmu z databáze
+for row in rows:
+    print(row[1], row[2])
 
 connection.close()
